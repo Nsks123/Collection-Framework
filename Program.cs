83dbaf0b924f2be4e6c0532f5af7eee99898116c
Nsks123 <nallamsunilkumar5@gmail.com>
2024-02-04 12:40:49 +0530

    [sunil] implemented some operations in list

--- Program.cs
+++ Program.cs
@@ -1,42 +1,59 @@
 ﻿using System;
-using System.Collections;
+using System.Collections.Generic;
+
+
 
 
 namespace Collection_Framework
 {
-    
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Stack stack = new Stack();
-            stack.Push(1);
-            stack.Push("sunil");
-            stack.Push(2);
-            stack.Push("kumar");
-            stack.Push(3);
-            stack.Push("nallam");
-            Console.WriteLine("The elements present after enqueue: ");
-            foreach (var item in stack)
-            {      
-                Console.WriteLine(item);
-            }
-            stack.Pop();
-            Console.WriteLine("The elements present after dequeue: ");
-            foreach (var item in stack)
+            List<String> list= new List<string>();
+            list.Add("ramesh");
+            list.Add("hyma");
+            list.Add("mahesh");
+            list.Add("sai");
+            list.Add("sunil");
+            list.Add("nallam");
+            Console.WriteLine("The elements present after adding: ");
+            foreach (var item in list)
             {
                 Console.WriteLine(item);
             }
-            stack.Peek();
-            Console.WriteLine("The elements present after peek is : {0} ",stack.Peek());
-           if(stack.Contains("sunil") == true)
+            list.Remove("sunil");
+            Console.WriteLine("elements present after remove: ");
+            foreach (var item in list)
             {
-                Console.WriteLine("Element available in the queue");
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("capacity of the list is "+list.Capacity);
+            list.RemoveAt(1);
+            Console.WriteLine("elements present after removeat: ");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            list.RemoveRange(1, 2);
+            Console.WriteLine("elements present after removerange: ");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
             }
             
-            stack.Clear();
-            Console.WriteLine("The elements present  after clear: {0} ",stack.Count);
+            list.RemoveAll(word => word.StartsWith("r", StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine("elements present after removeall: ");
             
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            list.Clear();
+            Console.WriteLine("no.of items present in the list after clear: " + list.Count);
+
+
             Console.ReadLine();
         }
     }
