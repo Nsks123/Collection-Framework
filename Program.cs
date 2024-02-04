bb63936c0d0aa32a2409c2d32fb885bebff99b4c
Nsks123 <nallamsunilkumar5@gmail.com>
2024-02-04 16:56:00 +0530

    [sunil] implemented linked list methods

--- Program.cs
+++ Program.cs
@@ -1,61 +1,52 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 
-
-
-
-namespace Collection_Framework
+class GFG
 {
+    static public void Main()
+    { 
+        LinkedList<String> list = new LinkedList<String>();
 
-    internal class Program
-    {
-        static void Main(string[] args)
+        list.AddLast("Sunil");
+        list.AddFirst("jash");
+        list.AddLast("Rohit");
+        list.AddFirst("anand");
+        list.AddFirst("shridhar");
+        list.AddLast("pradeep");
+
+        Console.WriteLine("Elements after adding:");
+        foreach (string str in list)
         {
-            ArrayList list = new ArrayList();
-            list.Add("ramesh");
-            list.Add(1);
-            list.Add("mahesh");
-            list.Add(2);
-            list.Add("sunil");
-            list.Add(3);
-            Console.WriteLine("The elements present after adding: ");
-            foreach (var item in list)
-            {
-                Console.WriteLine(item);
-            }
-            list.Remove("sunil");
-            Console.WriteLine("elements present after remove: ");
-            foreach (var item in list)
-            {
-                Console.WriteLine(item);
-            }
-            Console.WriteLine("capacity of the list is " + list.Capacity);
-            list.RemoveAt(1);
-            Console.WriteLine("elements present after removeat: ");
-            foreach (var item in list)
-            {
-                Console.WriteLine(item);
-            }
-            list.RemoveRange(1, 2);
-            Console.WriteLine("elements present after removerange: ");
-            foreach (var item in list)
-            {
-                Console.WriteLine(item);
-            }
-
-            //list.RemoveAll(word => word.StartsWith("r", StringComparison.OrdinalIgnoreCase));
-            Console.WriteLine("elements present after removeall: ");
-
-            foreach (var item in list)
-            {
-                Console.WriteLine(item);
-            }
-            list.Clear();
-            Console.WriteLine("no.of items present in the list after clear: " + list.Count);
-
-
-            Console.ReadLine();
+            Console.WriteLine(str);
         }
+        list.Remove(list.Last);
+        Console.WriteLine("Elements after removelast:");
+
+        foreach (string str in list)
+        {
+            Console.WriteLine(str);
+        }
+
+        list.Remove("Rohit");
+        Console.WriteLine("Elements after remove value:");
+
+        foreach (string str in list)
+        {
+            Console.WriteLine(str);
+        }
+        
+
+        list.RemoveFirst();
+        Console.WriteLine("Elements after removefirst:");
+
+        foreach (string str in list)
+        {
+            Console.WriteLine(str);
+        }
+        Console.WriteLine(list.Contains("sunil")==true);
+        ;
+        list.Clear();
+        Console.WriteLine("Number of students: "+ list.Count);
+        Console.ReadLine();
     }
 }
