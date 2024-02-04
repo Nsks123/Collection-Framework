95dd633eee1ccfb7ff24c58ac5bc31fcf268f530
Nsks123 <nallamsunilkumar5@gmail.com>
2024-02-04 23:11:34 +0530

    [sunil] implemented some operations in hashset

--- Program.cs
+++ Program.cs
@@ -5,48 +5,58 @@ class GFG
 {
     static public void Main()
     { 
-        LinkedList<String> list = new LinkedList<String>();
+        HashSet<String> list = new HashSet<String>();
 
-        list.AddLast("Sunil");
-        list.AddFirst("jash");
-        list.AddLast("Rohit");
-        list.AddFirst("anand");
-        list.AddFirst("shridhar");
-        list.AddLast("pradeep");
-
+        list.Add("sunil");
+        list.Add("jash");
+        list.Add("rohith");
+        list.Add("anand");
+        list.Add("shridhar");
+        list.Add("pradeep");
         Console.WriteLine("Elements after adding:");
         foreach (string str in list)
         {
             Console.WriteLine(str);
         }
-        list.Remove(list.Last);
-        Console.WriteLine("Elements after removelast:");
-
+        HashSet<String> set = new HashSet<String>();
+        set.Add("sunil");
+        set.Add("anil");
+        set.Add("mallikarjuna");
+        set.Add("rohith");
+        Console.WriteLine("Elements after adding:");
+        foreach (string str in set)
+        {
+            Console.WriteLine(str);
+        }
+        list.IntersectWith(set);
+        Console.WriteLine("total elements after intersectwith:");
         foreach (string str in list)
         {
             Console.WriteLine(str);
         }
-
-        list.Remove("Rohit");
-        Console.WriteLine("Elements after remove value:");
-
-        foreach (string str in list)
+        list.ExceptWith(set);
+        Console.WriteLine("elements after exceptwith:");
+        foreach (string str in set)
+        {
+            Console.WriteLine(str);
+        }
+        list.UnionWith(set);
+        Console.WriteLine("total elements after unionwith:");
+        foreach(string str in list)
         {
             Console.WriteLine(str);
         }
         
 
-        list.RemoveFirst();
-        Console.WriteLine("Elements after removefirst:");
+        list.Remove("mallikarjuna");
+        Console.WriteLine("Elements after remove:");
 
         foreach (string str in list)
         {
             Console.WriteLine(str);
         }
-        Console.WriteLine(list.Contains("sunil")==true);
-        ;
         list.Clear();
-        Console.WriteLine("Number of students: "+ list.Count);
+        Console.WriteLine("Number of elements: "+ list.Count);
         Console.ReadLine();
     }
 }
